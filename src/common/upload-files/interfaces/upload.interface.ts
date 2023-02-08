36191366cc8836a5ae37c94422e78e5966a1201c
dEN5 <moyadres_1@mail.ru>
2023-02-09 00:06:15 +0300

    add custom decorator 'isImage' for validate image mimetype

--- src/common/upload-files/interfaces/upload.interface.ts
+++ src/common/upload-files/interfaces/upload.interface.ts
@@ -1,6 +1,38 @@
-import { Contains, IsString } from 'class-validator';
-import { ReadStream } from 'fs';
 import { Stream } from 'stream';
+import {
+    registerDecorator,
+    ValidationOptions,
+    ValidatorConstraint,
+    ValidatorConstraintInterface,
+} from 'class-validator';
+import {  GraphQLUpload } from 'graphql-upload';
+import { ArgsType, Field } from '@nestjs/graphql';
+
+@ValidatorConstraint()
+class imageUpload implements ValidatorConstraintInterface {
+    async validate(image: IUpload) {
+        const { mimetype } = await image;
+
+        const mimeTypes: string[] = ['image/gif', 'image/png', 'image/jpeg'];
+        if (mimeTypes.includes(mimetype)) {
+            return true;
+        }
+        return false;
+    }
+}
+
+export function isImage(validationOptions?: ValidationOptions) {
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    return function (object: Object, propertyName: string) {
+        registerDecorator({
+            target: object.constructor,
+            propertyName: propertyName,
+            options: validationOptions,
+            constraints: [],
+            validator: imageUpload,
+        });
+    };
+}
 
 export interface IUpload {
     filename: string;
@@ -9,16 +41,19 @@ export interface IUpload {
     createReadStream: () => Stream;
 }
 
-export abstract class FileUploadDto {
-    @IsString()
-    public filename!: string;
+@ArgsType()
+export class FileUploadDto {
+    @Field(() => GraphQLUpload)
+    @isImage({ message: 'Only png,jpeg and gif images are allowed' })
+    file: IUpload;
+}
 
-    @IsString()
-    @Contains('image/png')
-    public mimetype!: string;
-
-    @IsString()
-    public encoding!: string;
-
-    public createReadStream: () => ReadStream;
+@ArgsType()
+export class FilesUploadDto {
+    @Field(() => GraphQLUpload)
+    @isImage({
+        message: 'Only png,jpeg and gif images are allowed',
+        each: true,
+    })
+    files: [IUpload];
 }
